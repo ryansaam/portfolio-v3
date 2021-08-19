2b8be33c72a75984724377bfda50ae592adfe15a
Ryan Sam <ryansam.inquiries@gmail.com>
2021-08-19 03:47:55 -0500

    add project cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Revolver from './components/Revolver/'
-import Cylinder, { CylinderData } from "./components/Cylinder"
+import Revolver from "./components/Revolver/"
+
+import aloomThumbnail from "./assets/aloom_thumbnail.mp4"
+import calendarWidget from "./assets/calendar_widget.jpeg"
+import minilatheThumbnail from "./assets/minilathe_thumbnail.mp4"
+import litphumThumbnail from "./assets/litphum_thumbnail.mp4"
+import atomarThumbnail from "./assets/atomar_thumbnail.jpeg"
+import bbookThumbnail from "./assets/bbook_thumbnail.png"
 
 interface BrandBarSpanProps {
   children: React.ReactNode,
@@ -64,31 +70,30 @@ const BrandBar = () => {
 }
 
 function App() {
-  const data: CylinderData[] = [
-    { 
-      backgroundColor: "red",
-      img: "https://cdn.dribbble.com/users/1769954/screenshots/15098252/media/28ef416a4c53cd65b88df05d50afc37f.png?compress=1&resize=1600x1200",
-      desc: "This project is too cool for your eyes!"
+  const data: ProjectData[] = [
+    {
+      media: aloomThumbnail,
+      name: "aloom"
     },
-    { 
-      backgroundColor: "green",
-      img: "https://cdn.dribbble.com/users/1769954/screenshots/15098252/media/28ef416a4c53cd65b88df05d50afc37f.png?compress=1&resize=1600x1200",
-      desc: "This project is too cool for your eyes!"
+    {
+      media: bbookThumbnail,
+      name: "BBOOK"
     },
-    { 
-      backgroundColor: "blue",
-      img: "https://cdn.dribbble.com/users/1769954/screenshots/15098252/media/28ef416a4c53cd65b88df05d50afc37f.png?compress=1&resize=1600x1200",
-      desc: "This project is too cool for your eyes!"
+    {
+      media: atomarThumbnail,
+      name: "Atomar"
     },
-    { 
-      backgroundColor: "white",
-      img: "https://cdn.dribbble.com/users/1769954/screenshots/15098252/media/28ef416a4c53cd65b88df05d50afc37f.png?compress=1&resize=1600x1200",
-      desc: "This project is too cool for your eyes!"
+    {
+      media: litphumThumbnail,
+      name: "litphum"
     },
-    { 
-      backgroundColor: "aqua",
-      img: "https://cdn.dribbble.com/users/1769954/screenshots/15098252/media/28ef416a4c53cd65b88df05d50afc37f.png?compress=1&resize=1600x1200",
-      desc: "This project is too cool for your eyes!"
+    {
+      media: minilatheThumbnail,
+      name: "MINI-LATHE"
+    },
+    {
+      media: calendarWidget,
+      name: "Calendar Widgets"
     }
   ]
 
@@ -96,23 +101,22 @@ function App() {
     <div className="App">
       <h1>EXPLORE My Cool Text 2109</h1>
       <BrandBar />
-      <div style={{width: "500px", height: "350px", position: "relative"}}>
+      <div style={{position: "relative"}}>
         <Revolver
           style={{
             position: "absolute",
             top: "0px",
             left: "0px",
-            // transform: "translateX(-50%)"
+            transform: "translateX(-60%)"
           }}
-          radius={100}
+          radius={244}
         >
           {
             data.map((obj, index) => {
-              return <Cylinder
+              return <ProjectCard
                         key={index}
-                        backgroundColor={obj.backgroundColor}
-                        // width={300} height={150}
-                        width={80 * (index+1)} height={80 * (index+1)}
+                        data={obj}
+                        onClick={() => {}}
                       />
             })
           }
@@ -121,5 +125,60 @@ function App() {
     </div>
   );
 }
+
+
+interface ProjectData {
+  media: string
+  name: string
+}
+interface ProjectProps {
+  data: ProjectData
+  onClick: () => void
+}
+
+const ProjectCard = React.forwardRef<HTMLDivElement, ProjectProps>(({ data }, ref) => {
+  return (
+    <div
+      ref={ref}
+      style={{
+        background: "radial-gradient(circle at top right, rgba(218,218,218,0.2) 0%, rgba(0,0,0,0) 100%)",
+        width: "300px",
+        height: "150px",
+        borderRadius: "25px",
+        display: "grid",
+        gridTemplateRows: "112px auto",
+        gridTemplateColumns: "225px auto",
+        cursor: "pointer"
+      }}
+    >
+      <div
+        style={{
+          width: "225px",
+          height: "112px",
+          borderRadius: "25px 0px 25px 0px",
+          overflow: "hidden",
+          position: "relative"
+        }}
+      >
+        <video
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover"
+          }}
+          poster={data.media}
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src={data.media} type="video/mp4" />
+        </video>
+      </div>
+      <h1 style={{fontSize: "42px", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", gridRow: "1 / 3", gridColumn: "2 / 3"}}>▶︎</h1>
+      <h1 style={{fontSize: "18px", paddingLeft: "30px", display: "flex", justifyContent: "center", flexDirection: "column"}}>{data.name}</h1>
+    </div>
+  )
+})
 
 export default App;
